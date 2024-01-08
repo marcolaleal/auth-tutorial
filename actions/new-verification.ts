6bd9801c3cf13cd2d60d34fb78adbadbba7e3d6c
marcolaleal <marcolaleal@gmail.com>
2024-01-07 23:12:21 -0300

    feat: complete email verification

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -4,7 +4,7 @@ import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { getVerificationTokenByToken } from "@/data/verification-token";
 
-export const newverification = async (token: string) => {
+export const newVerification = async (token: string) => {
   const existingToken = await getVerificationTokenByToken(token);
 
   if(!existingToken) {
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,18 +1,36 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 import { CardWrapper } from "./card-wrapper";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { newVerification } from "@/actions/new-verification";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string|undefined>();
+  const [success, setSuccess] = useState<string|undefined>();
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
   const onSubmit = useCallback( () => {
-    console.log(token);
+
+    if(!token) {
+      setError("Erro ao recuperar o token de validação");
+      return;
+    };
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error)
+      })
+      .catch(() => {
+        setError("Oops! Algo deu errado!")
+      })
   },[token]);
 
   useEffect(() => {
@@ -26,7 +44,11 @@ export const NewVerificationForm = () => {
       backButtonLabel="Ir para página de login"  
     >
       <div className="flex items-center w-full justify-center">
-        <BeatLoader />
+        {!success && !error && (
+          <BeatLoader />
+        )}
+        <FormSuccess message={success}/>
+        <FormError message={error}/>
       </div>
     </CardWrapper>
   )
