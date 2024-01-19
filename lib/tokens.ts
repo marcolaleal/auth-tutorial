ea4db18df5b226b7beea643cc95e68e2e96f3726
marcolaleal <marcolaleal@gmail.com>
2024-01-19 07:57:05 -0300

    feat: 2FA start implement

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,39 @@
+import crypto from "crypto";
 import {v4 as uuidv4} from "uuid";
 
 import { db } from "@/lib/db";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+  //cria o token de 6 digitos aleatorios
+  const token = crypto.randomInt(100_000,1_000_000).toString();
+
+  //ToDo mudar a validade do token para 15 min
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if(existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      expires,
+      token,
+    }
+  });
+
+  return twoFactorToken
+}
+
 
 export const generatePasswordResetToken = async (email:string) => {
   
