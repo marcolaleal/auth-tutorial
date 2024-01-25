8d3b666b63a973d75f7d6bfb0fbc3d10fa4f992f
marcolaleal <marcolaleal@gmail.com>
2024-01-25 08:06:49 -0300

    feat: userButton complete

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,19 @@
-"use client";
+ "use client";
 
-import  {signOut, useSession} from "next-auth/react";
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
 
 
 const SettingsPage =  () => {
-  const session = useSession();
+  const user = useCurrentUser();
 
   const onClick = () => {
-    signOut();
+    logout
   }
 
   return (
-    <div>
-      {JSON.stringify(session)}
-      
+    <div className="bg-white p-10 rounded-xl">
       <button type="submit" onClick={onClick}>
         Sign Out
       </button>
